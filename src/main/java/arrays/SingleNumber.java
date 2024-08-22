8d2e0066c69c416be36fffaba6532b6fdc910e60
Vinvith Kumar Mudugonda <vinvithkumar@gmail.com>
2024-08-21 21:56:56 -0500

    Arrays leetcode SingleNumber problem

--- src/main/java/arrays/SingleNumber.java
+++ src/main/java/arrays/SingleNumber.java
@@ -1,8 +1,42 @@
 package arrays;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SingleNumber {
 
-    public void returnSingleNumber(){
-        int[] inputArray = {2,2,1};
+    //Normal Approach - o(n) Time Complexity, o(n) Space Complexity
+    public int returnSingleNumber(int[] inputArray){
+        HashMap<Integer, Integer> noOfIntegerDuplicates = new HashMap<>();
+        for(Integer i : inputArray){
+            if(noOfIntegerDuplicates.containsKey(i)){
+                noOfIntegerDuplicates.put(i, noOfIntegerDuplicates.getOrDefault(i, 0)+1);
+            }else{
+                noOfIntegerDuplicates.put(i, 1);
+            }
+        }
+
+        for(Integer i : noOfIntegerDuplicates.keySet()){
+            if(noOfIntegerDuplicates.get(i) == 1){
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    //Efficient Approach - o(n) Time Complexity, o(1) Space Complexity
+    public int returnSingleInteger(int[] inputArray){
+        int singleNumber = 0;
+        for (int num : inputArray) {
+            singleNumber ^= num;
+        }
+        return singleNumber;
+    }
+
+    public static void main(String[] args){
+        int[] inputArray = {4,1,2,1,2};
+        SingleNumber singleNumber = new SingleNumber();
+        System.out.println(singleNumber.returnSingleNumber(inputArray));
+        System.out.println(singleNumber.returnSingleInteger(inputArray));
     }
 }
